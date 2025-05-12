75e1f8840c256378d7b48ad53b15e2e9fc44f51b
a_tahourynia <a_tahourynia@A-TAHOURYNIA>
2025-05-12 12:47:29 +0330

    add template

--- WebPrj_Angular/src/app/app.routes.ts
+++ WebPrj_Angular/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashbourdComponent } from './pages/dashbourd/dashbourd.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '',component:DashbourdComponent}
+];
